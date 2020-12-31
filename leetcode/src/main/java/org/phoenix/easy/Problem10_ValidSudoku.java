12050c78a0f97b6569b7d0418a1ab31c127316c2
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-12-31 21:52:20 +0530

    Added Problem 10 Valid Sudoku

---
+++ leetcode/src/main/java/org/phoenix/easy/Problem10_ValidSudoku.java
@@ -0,0 +1,59 @@
+/*
+* Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+* Each row must contain the digits 1-9 without repetition.
+* Each column must contain the digits 1-9 without repetition.
+* Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+*
+* Note:
+* A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+* Only the filled cells need to be validated according to the mentioned rules.
+* Example 1:
+* Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+* Output: true*/
+package org.phoenix.easy;
+
+import java.util.HashMap;
+
+public class Problem10_ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        // init data
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] columns = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashMap<Integer, Integer>();
+            columns[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+        }
+
+        // validate a board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    int n = (int) num;
+                    int box_index = (i / 3) * 3 + j / 3;
+
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+                    boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+                    if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ leetcode/src/test/java/org/phoenix/easy/Problem10_ValidSudokuTest.java
@@ -0,0 +1,25 @@
+package org.phoenix.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Problem10_ValidSudokuTest {
+
+    private final Problem10_ValidSudoku test = new Problem10_ValidSudoku();
+
+    @Test
+    void isValidSudoku() {
+        char[][] arr = new char[][]{{'5', '3', '.', '.', '7', '.', '.', '.', '.'}
+                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        assertTrue(test.isValidSudoku(arr));
+    }
+}
