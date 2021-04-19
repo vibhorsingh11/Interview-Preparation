168d68a5607aae2636b1b343fef06893b18d1fef
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-04-19 21:06:23 +0530

    added 02 Kth To Last

---
+++ CTCI/src/main/java/org/phoenix/interview/ctci/linkedLists/KthLast.java
@@ -0,0 +1,30 @@
+package org.phoenix.interview.ctci.linkedLists;
+
+public class KthLast {
+    public static int KthToLast(Node head, int k) {
+        if (head == null) return -1;
+        Node fast = head;
+        Node slow = head;
+        // Moving the counter less than k times
+        for (int i = 0; i < k && fast != null; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow.data;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        System.out.println(KthToLast(head, 3));
+
+        System.out.println(KthToLast(head, 2));
+        System.out.println(KthToLast(head, 1));
+    }
+}
